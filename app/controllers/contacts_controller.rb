442f35f0be9dc2bde2f7d737eec395a636691f4a
quietpochatok <bosroin@gmail.com>
2020-06-20 17:32:50 +0500

    feat: add contact form validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,28 @@
-class ContactsController < ApplicationController
-  def index
-  end
-  
-  def new
-  end
-
-  def create
-    #вывод введенных данных в хэш
-    # render plain: params[:contact].inspect
-    #######################################
-    #создание объекта с параметрами из браузера, бех сохранения
-    @contact = Contact.new(contact_params)
-    @contact.save
-  end
-
-  #метод для безопасного передачи введенных данных пользователем
-  private
-    def contact_params
-    params.require(:contact).permit(:name, :phone, :email, :message)
+  class ContactsController < ApplicationController
+    def index
     end
-  
-end
+    
+    def new
+    end
+
+    def create
+      #вывод введенных данных в хэш
+      # render plain: params[:contact].inspect
+      #######################################
+      #создание объекта с параметрами из браузера, бех сохранения
+      @contact = Contact.new(contact_params)
+        if @contact.valid?
+            @contact.save
+          else
+            render action: 'new'
+        end
+    end
+
+    #метод для безопасного передачи введенных данных пользователем
+    #нужно для @contact в следствии обновлении разработчиков руби
+    private
+      def contact_params
+      params.require(:contact).permit(:name, :phone, :email, :message)
+      end
+    
+  end
