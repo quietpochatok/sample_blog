0a94ad45c59a02af96b698b4d2c1700c74c8ea35
quietpochatok <bosroin@gmail.com>
2020-06-20 12:54:34 +0500

    feat: add show input data in form on page"contacts/new"

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,12 @@
 class ContactsController < ApplicationController
   def index
   end
+  
   def new
+  end
+
+  def create
+    render plain: params[:contact].inspect
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/'=> 'home#index'
   resources :articles
+  # get '/contacts/index' => 'contacts#index'
   get '/contacts/index' => 'contacts#index'
   get '/contacts/new' => 'contacts#new'
+  post '/contacts/' => 'contacts#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
