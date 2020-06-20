6605e9144b06d8eb4d5d7d048ad2855dacb21ba4
quietpochatok <bosroin@gmail.com>
2020-06-20 13:17:09 +0500

    refactor: change urls essence 'contacts' & comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get '/'=> 'home#index'
   resources :articles
   # get '/contacts/index' => 'contacts#index'
-  get '/contacts/index' => 'contacts#index'
-  get '/contacts/new' => 'contacts#new'
+  get '/contacts/' => 'contacts#index'
   post '/contacts/' => 'contacts#create'
+  get '/contacts/new' => 'contacts#new'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
