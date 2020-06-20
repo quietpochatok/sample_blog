3c911266001aefd61df1eaf72adee5c26a77fbb2
quietpochatok <bosroin@gmail.com>
2020-06-20 16:49:40 +0500

    feat: add save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,18 @@ class ContactsController < ApplicationController
   end
 
   def create
-    render plain: params[:contact].inspect
+    #вывод введенных данных в хэш
+    # render plain: params[:contact].inspect
+    #######################################
+    #создание объекта с параметрами из браузера, бех сохранения
+    @contact = Contact.new(contact_params)
+    @contact.save
   end
+
+  #метод для безопасного передачи введенных данных пользователем
+  private
+    def contact_params
+    params.require(:contact).permit(:name, :phone, :email, :message)
+    end
   
 end
