df66bbf647b9c443665e238c0f865d68e2bf1213
quietpochatok <bosroin@gmail.com>
2020-06-20 11:23:06 +0500

    feat: add controller & model contacts with table in db

---
+++ db/migrate/20200620061647_create_contacts.rb
@@ -0,0 +1,11 @@
+class CreateContacts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contacts do |t|
+      t.text :name
+      t.text :phone
+      t.text :address
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_145516) do
+ActiveRecord::Schema.define(version: 2020_06_20_061647) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.text "name"
+    t.text "phone"
+    t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
