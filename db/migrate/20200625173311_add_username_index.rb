ebfb0236783d5b1e09f46b6d5570a9de535e874e
quietpochatok <bosroin@gmail.com>
2020-06-25 22:39:36 +0500

    feat: add user_name column to users table & username_index

---
+++ db/migrate/20200625173311_add_username_index.rb
@@ -0,0 +1,5 @@
+class AddUsernameIndex < ActiveRecord::Migration[6.0]
+  def change
+    add_index :users, :user_name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_165526) do
+ActiveRecord::Schema.define(version: 2020_06_25_173311) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "title"
@@ -59,8 +59,10 @@ ActiveRecord::Schema.define(version: 2020_06_24_165526) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   add_foreign_key "comments", "articles"
