2f9087b73d22a464d2b0f4f7b2341bf89edfbb0e
quietpochatok <bosroin@gmail.com>
2020-07-01 21:50:39 +0500

    feat: add article contact spec

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,15 @@
+  
+# require 'rails_helper'
+
+# describe "something" do 
+#   it "does something" do
+#     #/////
+#   end
+# end
+
+require 'rails_helper'
+
+describe Contact do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :phone }
+end
