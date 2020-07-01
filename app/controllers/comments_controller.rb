5e9eb63403df8f39d33f6e787cf282c94d5ab2c9
quietpochatok <bosroin@gmail.com>
2020-07-01 18:12:31 +0500

    feat: add output nickname when viewing and create a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,23 @@
   class CommentsController < ApplicationController
-     before_action :authenticate_user!, only: [:create]
+    before_action :authenticate_user!, only: [:create]
+    
     def create
       @article = Article.find(params[:article_id])
-      @article.comments.create(comment_params)
-
-      redirect_to article_path(@article)
+      #@article.comments.create(comment_params)
+      @comment = @article.comments.new do |c|
+        c.author = current_user.username
+        c.body = comment_params[:body]
+      end
+          if @comment.save
+          redirect_to article_path(@article)
+          else
+              render action: 'create'
+          end
     end
 
     private
         def comment_params
         params.require(:comment).permit(:author, :body)
         end
+        
   end
