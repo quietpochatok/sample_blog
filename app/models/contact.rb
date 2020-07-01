0efed9770193cb3bd4a79506d0f5fc894de090fb
quietpochatok <bosroin@gmail.com>
2020-07-01 22:17:37 +0500

    refactor: minor change

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,4 @@
 class Contact < ApplicationRecord
-  # validates :email, presence: true
-  # validates :message, presence: true
-
   validates :name, presence: true
   validates :phone, presence: true
 end
