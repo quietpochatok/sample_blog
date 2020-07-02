1faf5731f0cea5e791ce8e0d820feb114c68204f
quietpochatok <bosroin@gmail.com>
2020-07-02 21:30:23 +0500

    upd: add test 'associations'&'#subject' in article_spec.rb

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
-describe Article do
-  describe "validations" do
-    it {should validate_presence_of :title}
-    it {should validate_presence_of :text}
-  end
+  describe Article do
+    describe "validations" do
+      it {should validate_presence_of :title}
+      it {should validate_presence_of :text}
+    end
 
-  describe "associations" do
-    it {should have_many :comments}
-  end
+    describe "associations" do
+      it {should have_many :comments}
+    end
 
+    describe "#subject" do
+      it  "returns the article title" do
+        #arrange&act
+        article = create(:article, title:'Lorem')
+        #asssert
+        expect(article.subject).to eq 'Lorem'
+      end
+    end
 end
